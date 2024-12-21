0692ab304dc8683e71f2731b98b1a4b611c5d941
NishantSwat <nayanishant.info@gmail.com>
2024-12-22 02:18:03 +0530

    UI for chat

--- frontend/src/pages/main/main.jsx
+++ frontend/src/pages/main/main.jsx
@@ -1,12 +1,50 @@
-import NavBar from '../../components/navbar/navbar'
+import NavBar from "../../components/navbar/navbar";
+import "./main.css";
+import { useSelector } from "react-redux";
 
 const Main = () => {
+  const name = useSelector((state) => state.user.user.name);
+
+  const initials = name
+    ? name
+        .split(" ")
+        .map((word) => word[0])
+        .join("")
+    : "";
+
   return (
     <>
-      <NavBar />
-      <div>Main</div>
+      <div className="main-wrapper">
+        <NavBar />
+        <div className="wrapper-right">
+          <div className="response-container">
+            <div>
+              <div className="user">
+                <div className="name-logo">
+                  <p className="text-clr">{initials}</p>
+                </div>
+                <p className="text-clr">Hi, How are you?</p>
+              </div>
+              <div className="model">
+                <i className="fa-solid fa-wand-magic-sparkles"></i>
+                <p className="text-clr">
+                  I am doing well, thank you for asking! How are you today?
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="input-container">
+            <textarea name="search"></textarea>
+            <div className="send-btn">
+              <button>
+                <i class="fa-solid fa-paper-plane"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
